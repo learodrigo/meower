f372f56624601b0c5d8ddda49517093cc0ae82a6
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2020-08-23 18:30:01 +0200

    Add cors and mongodb connection

--- client/client.js
+++ client/client.js
@@ -1,24 +1,29 @@
+const API_URL = 'http://localhost:5000/mews'
 const form = document.querySelector('form')
 const loadingElement = document.querySelector('.loading')
-const API_URL = 'http://localhost:5000/mews'
 
 loadingElement.style.display = 'none'
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
     const formData = new FormData(form)
-    const name = formData.get('name')
     const content = formData.get('content')
+    const name = formData.get('name')
+
     const mew = { name, content }
 
     form.style.display = 'none'
     loadingElement.style.display = 'block'
 
     fetch(API_URL, {
-        method: 'POST',
-        body: JSON.stringify(mew),
         header: {
             'content-type': 'application/json'
-        }
+        },
+        body: JSON.stringify(mew),
+        method: 'POST'
+    })
+    .then(response => response.json())
+    .then(createdMew => {
+        console.log(createdMew)
     })
 })
