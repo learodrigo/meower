48d0d88579f3c3821177540609ec778ffd918306
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2020-08-23 19:56:56 +0200

    Add all comments after submit (#6)

--- client/client.js
+++ client/client.js
@@ -1,8 +1,12 @@
 const API_URL = 'http://localhost:5000/mews'
+
 const form = document.querySelector('form')
 const loadingElement = document.querySelector('.loading')
+const mewsElement = document.querySelector('.mews')
 
-loadingElement.style.display = 'none'
+loadingElement.style.display = 'block'
+
+listAllMews()
 
 form.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -24,6 +28,37 @@ form.addEventListener('submit', (e) => {
     })
     .then(response => response.json())
     .then(createdMew => {
-        console.log(createdMew)
+        setTimeout(() => {
+            form.reset()
+            form.style.display = 'block'
+        }, 30000)
+        loadingElement.style.display = 'none'
+        listAllMews()
     })
 })
+
+function listAllMews () {
+    mewsElement.innerHTML = ''
+
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(mews => {
+            mews.reverse()
+            mews.forEach(mew => {
+                const header = document.createElement('h3')
+                const content = document.createElement('p')
+                const date = document.createElement('small')
+                header.textContent = mew.name
+                content.textContent = mew.content
+                date.textContent = new Date(mew.created)
+
+                const div = document.createElement('div')
+                div.appendChild(header)
+                div.appendChild(content)
+                div.appendChild(date)
+
+                mewsElement.appendChild(div)
+            })
+            loadingElement.style.display = 'none'
+        })
+}
